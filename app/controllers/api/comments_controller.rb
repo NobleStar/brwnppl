6098ffefe637972a45c8429f7b48c1edfadc627b
Jasdeep Singh <jasdeep.singh@mosaic.com>
2012-10-04 20:14:06 -0400

    fixed the small typo

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -3,7 +3,7 @@ class Api::CommentsController < Api::BaseController
   def create
     @story = Story.find(params[:story_id])
     @story.comments << Comment.create(:content => params[:content], :user_id => current_user.id)
-    render json: @story.to_json(:methods => :likes_count, :avatar)
+    render json: @story.to_json(:methods => [:likes_count, :avatar])
   end
 
 end
