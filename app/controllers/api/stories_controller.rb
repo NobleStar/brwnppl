75a15c4561319717622c0f83107ba9df466d3fce
Jasdeep Singh <narang.jasdeep@gmail.com>
2013-01-13 01:16:30 -0500

    more fixes

--- app/controllers/api/stories_controller.rb
+++ app/controllers/api/stories_controller.rb
@@ -21,7 +21,7 @@ class Api::StoriesController < Api::BaseController
     @story.user = current_user
     @story.oauth_token = session[:oauth_token]
     if @story.save
-      render json: { story: @story.to_json, header: 'You are awesome!', messages: ['You are awesome! Keep posting more stuff!'], saved: true }
+      render json: { story: @story.to_json, header: 'Thanks for sharing!', messages: ['Feel free to take a screen shot of this message, print it out, and add it to your scrap book.'], saved: true }
     else
       render json: { header: 'Uh-oh! Let\'s fix this up buddy, shall we?', messages: @story.errors.full_messages, saved: false }
     end
