b51de4e6796d5f1b1c40a7ac90421ada88f17f22
Jasdeep Singh <narang.jasdeep@gmail.com>
2013-01-28 21:10:42 -0500

    added support for fullscreen youtube videos

--- lib/brwnppl/preview/youtube.rb
+++ lib/brwnppl/preview/youtube.rb
@@ -1,7 +1,7 @@
 class Brwnppl::Preview::Youtube
 
   def html(graph_object)
-    "<iframe width='530' height='300' src='#{graph_object.video}' frameborder='0' allowfullscreen></iframe>"
+    "<iframe width='530' height='300' src='#{graph_object.video}&fs=1' frameborder='0' allowfullscreen webkitallowfullscreen mozallowfullscreen></iframe>"
   end
 
 end
